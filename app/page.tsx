f224f661e42d66cdf804737e1db6fd224c4460a9
Aviran Abramov <aviranabramov@gmail.com>
2026-01-11 07:20:09 +0200

    Install Theme Provider from ShadCN

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import { ModeToggle } from "@/components/mode-toggle";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div>
       <h1 className="text-2xl">App</h1>
+      <ModeToggle />
     </div>
   );
 }
