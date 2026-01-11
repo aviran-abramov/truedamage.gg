b566dfbbd5e902d6835530fe2eedb7d8f02f9c1b
Aviran Abramov <aviranabramov@gmail.com>
2026-01-11 12:52:33 +0200

    Add border-bottom to the header

--- components/AppHeader.tsx
+++ components/AppHeader.tsx
@@ -4,7 +4,7 @@ import { ModeToggle } from './mode-toggle'
 
 export const AppHeader = () => {
     return (
-        <header className="px-6 py-2.5 bg-white dark:bg-slate-900">
+        <header className="px-6 py-2.5 bg-[#F1F1F5] dark:bg-slate-900 border-b">
             <div className="flex items-center justify-between max-w-7xl m-auto">
                 <Link href="/" className="font-extrabold tracking-tight text-xl">TrueDamage.gg</Link>
                 <Navbar />
