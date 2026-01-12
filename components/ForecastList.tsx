76381e1aebbed9490c8d151e833eb4f9e8fd1907
Aviran Abramov <aviranabramov@gmail.com>
2026-01-12 13:39:31 +0200

    Move forecasts data to its own file

--- components/ForecastList.tsx
+++ components/ForecastList.tsx
@@ -1,41 +1,6 @@
-import { MatchProps } from "@/lib/types/match"
 import { Forecast } from "./Forecast"
+import { ForecastListData } from "@/lib/data/forecasts"
 
-const ForecastListData: MatchProps[] = [
-    {
-        id: "1",
-        matchDate: "2026-01-12",
-        matchTime: "11:00",
-        gameName: "League of Legends",
-        leagueName: "LoL Champions Korea Challengers League (LCK CL) 2026 Kickoff",
-        teamAName: "BIG Academy",
-        teamBName: "BRION Challengers",
-        bestOf: 3,
-        forecastedWinnerName: "BRION Challengers",
-    },
-    {
-        id: "2",
-        matchDate: "2026-01-13",
-        matchTime: "14:00",
-        gameName: "League of Legends",
-        leagueName: "LoL Champions Korea Challengers League (LCK CL) 2026 Kickoff",
-        teamAName: "BNK FEARX Youth",
-        teamBName: "T1 Esports Academy",
-        bestOf: 3,
-        forecastedWinnerName: "T1 Esports Academy",
-    },
-    {
-        id: "3",
-        matchDate: "2026-01-14",
-        matchTime: "16:00",
-        gameName: "League of Legends",
-        leagueName: "LoL Champions Korea Challengers League (LCK CL) 2026 Kickoff",
-        teamAName: "All for One Gaming",
-        teamBName: "Second Time Alive",
-        bestOf: 5,
-        forecastedWinnerName: "All for One Gaming",
-    }
-];
 
 export const ForecastList = () => {
     return (
