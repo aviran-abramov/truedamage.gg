6f2447b22887a486af675ca7a2450dfb1b5ef250
Aviran Abramov <aviranabramov@gmail.com>
2026-01-12 14:00:30 +0200

    Create interface for team

---
+++ lib/types/team.ts
@@ -0,0 +1,12 @@
+export interface TeamProps {
+    id: string;
+    name: string;
+    slug: string;
+    shortName: string;
+    region: string;
+    country: string;
+    game: string;
+    logoUrl: string;
+    createdAt: string;
+    updatedAt: string;
+}
