38456938422d9af22b96b9079128c2795820680e
Aviran <aviranabramov@gmail.com>
2026-01-20 14:46:25 +0200

    Add max-width and mx-auto to Admin Panel

--- components/AppHeader.tsx
+++ components/AppHeader.tsx
@@ -46,18 +46,20 @@ const adminLinks = [
 const AdminBar = () => {
 
     return (
-        <div className='px-6 py-0.5 bg-red-400 dark:bg-red-900 flex items-center border-b shadow-md'>
-            <h2 className='text-md font-semibold'>Admin Panel</h2>
+        <div className='px-6 py-0.5 bg-red-400 dark:bg-red-900 border-b shadow-md'>
+            <div className='flex items-center max-w-7xl mx-auto'>
+                <h2 className='text-md font-semibold'>Admin Panel</h2>
 
-            <ul className='flex items-center gap-2 ml-4'>
-                {adminLinks.map((link) => (
-                    <li key={link.href}>
-                        <Button variant="link" asChild className="px-0 font-semibold">
-                            <Link href={link.href} className="px-0">{link.label}</Link>
-                        </Button>
-                    </li>
-                ))}
-            </ul>
+                <ul className='flex items-center gap-2 ml-4'>
+                    {adminLinks.map((link) => (
+                        <li key={link.href}>
+                            <Button variant="link" asChild className="px-0 font-semibold">
+                                <Link href={link.href} className="px-0">{link.label}</Link>
+                            </Button>
+                        </li>
+                    ))}
+                </ul>
+            </div>
         </div>
     )
 }
