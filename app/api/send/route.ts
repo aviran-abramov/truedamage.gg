23e9d857a192a9ef1f149051d89fea3b2d195226
Aviran <aviranabramov@gmail.com>
2026-01-31 14:42:08 +0200

    Add React Email and create first email template

--- app/api/send/route.ts
+++ app/api/send/route.ts
@@ -1,4 +1,6 @@
+import GithubAccessTokenEmail from '@/components/email-template';
 import { Resend } from 'resend';
+
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
@@ -7,19 +9,20 @@ export async function POST() {
         const { data, error } = await resend.emails.send({
             from: 'Acme <onboarding@resend.dev>',
             to: [`${process.env.MY_EMAIL_ADDRESS}`],
-            subject: 'Hello world',
+            subject: 'New Template',
             // react: EmailTemplate({ firstName: 'John' }),
-            html: `<h1>Hello World2</h1>`
+            // html: `<h1>Hello World2</h1>`
+            react: GithubAccessTokenEmail({ username: "johndoe" })
         });
 
         if (error) {
+            console.log(error);
             return Response.json({ error }, { status: 500 });
         }
 
         return Response.json(data);
     } catch (error) {
         console.log(error);
-
         return Response.json({ error }, { status: 500 });
     }
 }
