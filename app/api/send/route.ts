047c4ebbd31e2ac7d5756847c32a1a702cb768fa
Aviran <aviranabramov@gmail.com>
2026-01-31 13:03:20 +0200

    Install and configure Resend

---
+++ app/api/send/route.ts
@@ -0,0 +1,25 @@
+import { Resend } from 'resend';
+
+const resend = new Resend(process.env.RESEND_API_KEY);
+
+export async function POST() {
+    try {
+        const { data, error } = await resend.emails.send({
+            from: 'Acme <onboarding@resend.dev>',
+            to: [`${process.env.MY_EMAIL_ADDRESS}`],
+            subject: 'Hello world',
+            // react: EmailTemplate({ firstName: 'John' }),
+            html: `<h1>Hello World2</h1>`
+        });
+
+        if (error) {
+            return Response.json({ error }, { status: 500 });
+        }
+
+        return Response.json(data);
+    } catch (error) {
+        console.log(error);
+
+        return Response.json({ error }, { status: 500 });
+    }
+}
