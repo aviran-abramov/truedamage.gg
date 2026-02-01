bffe1f9cdb5a105200be74e595f7266df765d883
Aviran Abramov <aviranabramov@gmail.com>
2026-02-01 10:31:11 +0200

    Split join now and back to login into components

--- components/layout/AppHeader/Auth/AuthModalFooter.tsx
+++ components/layout/AppHeader/Auth/AuthModalFooter.tsx
@@ -14,26 +14,48 @@ export const AuthModalFooter = ({
 
     return (
         authModalToShow === "signIn" ? (
-            <div className='flex items-center justify-center gap-1'>
-                <p>Not a member?</p>
-                <Link
-                    onClick={() => onAuthModalToShowClick("signUp")}
-                    href="#"
-                    className='hover:underline text-blue-400 flex items-center justify-end gap-1 underline-offset-1'
-                >
-                    Join now
-                    <ArrowRight />
-                </Link>
-            </div>
+            <JoinNow onAuthModalToShowClick={onAuthModalToShowClick} />
         ) : (
-            <Link
-                onClick={() => onAuthModalToShowClick("signIn")}
-                href="#"
-                className='hover:underline text-blue-400 flex items-center justify-start gap-1 underline-offset-1'
-            >
-                <ArrowLeft />
-                Back to sign in
-            </Link>
+            <BackToSignIn onAuthModalToShowClick={onAuthModalToShowClick} />
         )
+    )
+}
+
+interface JoinNowProps {
+    onAuthModalToShowClick: (type: AuthModalType) => void;
+}
+
+const JoinNow = ({ onAuthModalToShowClick }: JoinNowProps) => {
+
+    return (
+        <div className='flex items-center justify-center gap-1'>
+            <p>Not a member?</p>
+            <Link
+                onClick={() => onAuthModalToShowClick("signUp")}
+                href="#"
+                className='hover:underline text-blue-400 flex items-center justify-end gap-1 underline-offset-1'
+            >
+                Join now
+                <ArrowRight />
+            </Link>
+        </div>
+    )
+}
+
+interface BackToSignInProps {
+    onAuthModalToShowClick: (type: AuthModalType) => void;
+}
+
+const BackToSignIn = ({ onAuthModalToShowClick }: BackToSignInProps) => {
+
+    return (
+        <Link
+            onClick={() => onAuthModalToShowClick("signIn")}
+            href="#"
+            className='hover:underline text-blue-400 flex items-center justify-start gap-1 underline-offset-1'
+        >
+            <ArrowLeft />
+            Back to sign in
+        </Link>
     )
 }
