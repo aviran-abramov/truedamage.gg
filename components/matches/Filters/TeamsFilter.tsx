36f1855b0052785616c00575b91975868cfc45b4
Aviran <aviranabramov@gmail.com>
2026-02-05 20:43:11 +0200

    Add Search For Teams input group in TeamsFilter

--- components/matches/Filters/TeamsFilter.tsx
+++ components/matches/Filters/TeamsFilter.tsx
@@ -1,19 +1,26 @@
+"use client";
+
 import { Team } from "@/lib/generated/prisma/client";
 import { FilterTitle } from "./FilterTitle";
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
+import { useState } from "react";
+import { InputGroup, InputGroupAddon, InputGroupInput } from "@/components/ui/input-group";
+import { Search } from "lucide-react";
 
 interface TeamsFilterProps {
     teams: Team[];
 }
 
 export const TeamsFilter = ({ teams }: TeamsFilterProps) => {
+    const [visibleTeams, setVisibleTeams] = useState(teams.slice(0, 6));
 
     return (
-        <div className="rounded-sm p-4 text-black dark:text-white text-sm bg-[#F1F1F5] dark:bg-[#191921]">
+        <div className="rounded-sm p-4 text-black dark:text-white text-sm bg-[#F1F1F5] dark:bg-[#191921] space-y-3 flex flex-col w-full">
             <FilterTitle>Teams</FilterTitle>
+
             <ul className="flex items-center flex-wrap gap-2">
-                {teams.slice(0, 6).map((team) => (
+                {visibleTeams.map((team) => (
                     <li key={team.id}>
                         <Button variant={"outline"} className="cursor-pointer rounded-sm">
                             <Image
@@ -28,6 +35,14 @@ export const TeamsFilter = ({ teams }: TeamsFilterProps) => {
                     </li>
                 ))}
             </ul>
+
+            <InputGroup className="max-w-xs rounded-sm">
+                <InputGroupInput placeholder="Search for teams" className="w-full" />
+                <InputGroupAddon>
+                    <Search />
+                </InputGroupAddon>
+                <InputGroupAddon align="inline-end">{teams.length} results</InputGroupAddon>
+            </InputGroup>
         </div>
     )
 }
