85fe8c50fcd3eee1f08fe7df5d91205d346a98cf
Aviran Abramov <aviranabramov@gmail.com>
2026-02-06 10:26:06 +0200

    Make SearchByTeamFilter function with combobox

--- components/matches/Filters/TeamsFilter.tsx
+++ components/matches/Filters/TeamsFilter.tsx
@@ -5,8 +5,7 @@ import { FilterTitle } from "./FilterTitle";
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
 import { useState } from "react";
-import { InputGroup, InputGroupAddon, InputGroupInput } from "@/components/ui/input-group";
-import { Search } from "lucide-react";
+import { Combobox, ComboboxContent, ComboboxEmpty, ComboboxInput, ComboboxItem, ComboboxList } from "@/components/ui/combobox";
 
 interface TeamsFilterProps {
     teams: Team[];
@@ -15,13 +14,21 @@ interface TeamsFilterProps {
 export const TeamsFilter = ({ teams }: TeamsFilterProps) => {
     const [visibleTeams, setVisibleTeams] = useState(teams.slice(0, 6));
 
+    const handleSearchByFilterPick = (id: string) => {
+        const team = teams.find(t => t.id === id);
+
+        if (team) {
+            setVisibleTeams(prevState => [...prevState, team])
+        }
+    }
+
     return (
         <div className="rounded-sm p-4 text-black dark:text-white text-sm bg-[#F1F1F5] dark:bg-[#191921] space-y-3 flex flex-col w-full">
             <FilterTitle>Teams</FilterTitle>
 
             <VisibleTeamsList teams={visibleTeams} />
 
-            <SearchByTeamFilter teams={teams} />
+            <SearchByTeamFilter teams={teams} onSearchByFilterPick={handleSearchByFilterPick} />
         </div>
     )
 }
@@ -54,17 +61,35 @@ const VisibleTeamsList = ({ teams }: VisibleTeamsListProps) => {
 
 interface SearchByTeamFilterProps {
     teams: Team[];
+    onSearchByFilterPick: (id: string) => void
 }
 
-const SearchByTeamFilter = ({ teams }: SearchByTeamFilterProps) => {
+const SearchByTeamFilter = ({ teams, onSearchByFilterPick }: SearchByTeamFilterProps) => {
 
     return (
-        <InputGroup className="max-w-xs rounded-sm">
-            <InputGroupInput placeholder="Search for teams" className="w-full" />
-            <InputGroupAddon>
-                <Search />
-            </InputGroupAddon>
-            <InputGroupAddon align="inline-end">{teams.length} results</InputGroupAddon>
-        </InputGroup>
+        <Combobox items={teams}>
+            <ComboboxInput placeholder="Search for Teams" />
+            <ComboboxContent>
+                <ComboboxEmpty>No items found.</ComboboxEmpty>
+                <ComboboxList>
+                    {(team: Team) => (
+                        <ComboboxItem key={team.id} value={team.name} onClick={() => onSearchByFilterPick(team.id)} >
+                            <div className="flex items-center justify-between">
+                                <p>{team.name}</p>
+                            </div>
+                        </ComboboxItem>
+                    )}
+                </ComboboxList>
+            </ComboboxContent>
+        </Combobox>
+
+        // TODO: Make it look like this input group
+        // <InputGroup className="max-w-xs rounded-sm">
+        //     <InputGroupInput placeholder="Search for teams" className="w-full" />
+        //     <InputGroupAddon>
+        //         <Search />
+        //     </InputGroupAddon>
+        //     <InputGroupAddon align="inline-end">{teams.length} results</InputGroupAddon>
+        // </InputGroup>
     )
 }
