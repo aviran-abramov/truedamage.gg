68d43b25270be14248d91b9249541df4396b9aea
Aviran <aviranabramov@gmail.com>
2026-02-07 11:29:41 +0200

    Add session file and replace hard-coded sessions

--- components/layout/AppContainer.tsx
+++ components/layout/AppContainer.tsx
@@ -7,7 +7,7 @@ interface AppContainerProps {
 export const AppContainer = ({ children }: AppContainerProps) => {
 
     return (
-        <div className="max-w-7xl mx-auto px-6 py-10 flex flex-col items-center">
+        <div className="max-w-6xl mx-auto px-6 py-10 flex flex-col items-center">
             {children}
         </div>
     )
--- components/layout/AppHeader/AppHeader.tsx
+++ components/layout/AppHeader/AppHeader.tsx
@@ -3,11 +3,10 @@ import { auth } from '@/lib/auth';
 import { AdminBar } from './AdminNavigation/AdminBar';
 import { UserRole } from '@/lib/generated/prisma/enums';
 import { UserBar } from './UserNavigation/UserBar';
+import { getSession } from '@/lib/auth-session';
 
 export async function AppHeader() {
-    const session = await auth.api.getSession({
-        headers: await headers(),
-    });
+    const session = await getSession();
 
     const isSignedIn = !!session;
     const isAdmin = session?.user.role === UserRole.ADMIN;
