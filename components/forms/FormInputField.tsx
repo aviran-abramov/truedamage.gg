b6fed1acfab86d62fd529fb7db30130a3924a4a0
Aviran Abramov <aviranabramov@gmail.com>
2026-02-11 15:45:32 +0200

    feat: add FormInputField component

---
+++ components/forms/FormInputField.tsx
@@ -0,0 +1,43 @@
+import { Control, Controller } from "react-hook-form";
+import { Field, FieldContent, FieldError, FieldLabel } from "../ui/field";
+import { Input } from "../ui/input";
+
+interface FormInputFieldProps {
+    control: Control<any>;
+    name: string;
+    label: string;
+    type?: string
+    placeholder?: string;
+}
+
+export const FormInputField = ({
+    control,
+    name,
+    label,
+    type = "text",
+    placeholder
+}: FormInputFieldProps) => {
+
+    return (
+        <Controller
+            name={name}
+            control={control}
+            render={({ field, fieldState }) => (
+                <Field data-invalid={fieldState.invalid}>
+                    <FieldContent>
+                        <FieldLabel htmlFor={field.name}>{label}</FieldLabel>
+                        <Input
+                            type={type}
+                            placeholder={placeholder}
+                            {...field} id={field.name}
+                            aria-invalid={fieldState.invalid}
+                        />
+                        {fieldState.invalid && (
+                            <FieldError errors={[fieldState.error]} />
+                        )}
+                    </FieldContent>
+                </Field>
+            )}
+        />
+    )
+}
