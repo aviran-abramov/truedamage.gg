560ebc5bcdf17918206dacfca25c49eb306a6a27
Aviran Abramov <aviranabramov@gmail.com>
2026-02-11 18:19:46 +0200

    refactor: use FormSelect,InputField in CreateTeam

--- components/forms/admin/teams/create/CreateTeam.tsx
+++ components/forms/admin/teams/create/CreateTeam.tsx
@@ -1,12 +1,14 @@
 "use client";
 
-import { FormField } from "@/components/forms/FormField";
-import { FormSelectGameField } from "@/components/forms/FormSelectGameField";
+import { FormInputField } from "@/components/forms/FormInputField";
+import { FormSelectField } from "@/components/forms/FormSelectField";
 import { Button } from "@/components/ui/button";
-import { CardFooter } from "@/components/ui/card";
+import { FieldSeparator } from "@/components/ui/field";
 import { createTeam } from "@/lib/actions/teams";
 import { Game } from "@/lib/generated/prisma/client";
-import { TeamSchema } from "@/lib/validators/team";
+import { TeamFormData, TeamSchema } from "@/lib/validators/team";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
 import { toast } from "sonner";
 
 interface CreateTeamFormProps {
@@ -14,68 +16,72 @@ interface CreateTeamFormProps {
 }
 
 export function CreateTeamForm({ games }: CreateTeamFormProps) {
-    const handleCreateTeam = async (formData: FormData) => {
-        const newTeam = {
-            gameName: formData.get("gameName") as string,
-            name: formData.get("name") as string,
-            countryName: formData.get("countryName") as string,
-            countryCode: formData.get("countryCode") as string,
-        };
-
-        const result = TeamSchema.safeParse(newTeam);
-        if (!result.success) {
-            toast.warning(result.error.issues[0].message, { position: "top-center" });
-            return;
+    const form = useForm<TeamFormData>({
+        resolver: zodResolver(TeamSchema),
+        defaultValues: {
+            gameName: "",
+            name: "",
+            countryName: "",
+            countryCode: ""
         }
+    });
 
-        const response = await createTeam(newTeam);
-        if (response?.error) {
-            toast.error(response.error, { position: "top-center" });
-            return;
+    const onSubmit = async (data: TeamFormData) => {
+        const result = await createTeam(data);
+
+        if (result.success) {
+            form.reset();
+            toast.success("Team created successfully!", { position: "top-center" });
+            setTimeout(() => {
+                window.location.href = "/";
+            }, 1500);
+        } else {
+            toast.error("Failed to create team.", { position: "top-center" });
         }
+    };
 
-        toast.success("Team created successfully!", { position: "top-center" });
-        setTimeout(() => {
-            window.location.href = "/";
-        }, 1500);
-    }
-
+    const gameSelectItems = games.map((game) => {
+        return {
+            id: game.id,
+            label: game.name,
+            value: game.name
+        }
+    });
     return (
-        <form action={handleCreateTeam} className="flex flex-col gap-4">
-            <FormSelectGameField
-                label="Game"
-                name="gameName"
+        <form onSubmit={form.handleSubmit(onSubmit)} className="flex flex-col gap-4">
+            <FormSelectField
+                control={form.control}
+                controllerName="gameName"
+                fieldLabel="Game"
                 placeholder="Select a game"
-                title="Games"
-                games={games}
+                selectLabel="Games"
+                items={gameSelectItems}
             />
 
-            <FormField
-                name="name"
-                label="Name"
-                type="text"
+            <FormInputField
+                control={form.control}
+                controllerName="name"
+                fieldLabel="Name"
                 placeholder="Team Vitality"
             />
 
-            <FormField
-                name="countryName"
-                label="Country Name"
-                type="text"
+            <FormInputField
+                control={form.control}
+                controllerName="countryName"
+                fieldLabel="Country Name"
                 placeholder="South Korea"
             />
 
-            <FormField
-                name="countryCode"
-                label="Country Code"
-                type="text"
+            <FormInputField
+                control={form.control}
+                controllerName="countryCode"
+                fieldLabel="Country Code"
                 placeholder="KR"
             />
 
-            <CardFooter className="flex flex-col gap-2 border-t">
-                <Button type="submit" className="w-full cursor-pointer">
-                    Create Team
-                </Button>
-            </CardFooter>
+            <FieldSeparator />
+
+            <Button type="submit" className="w-full cursor-pointer">Create Team</Button>
         </form>
     )
 }
