4cf161261c2e0c9a83bb097a403277cc5a3d43bb
Aviran <aviranabramov@gmail.com>
2026-02-12 20:52:00 +0200

    refactor: use Zod + RHF in ForgotPasswordForm

--- components/forms/auth/ForgotPassword.tsx
+++ components/forms/auth/ForgotPassword.tsx
@@ -1,41 +1,40 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
-import { signIn } from "@/lib/actions/auth";
-import { FormField } from "../FormField";
+import { forgotPassword } from "@/lib/actions/auth";
 import { toast } from "sonner";
-import { ForgotPasswordSchema } from "@/lib/validators/auth/forgotPassword";
+import { ForgotPasswordFormData, ForgotPasswordSchema } from "@/lib/validators/auth/forgotPassword";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { FormInputField } from "../FormInputField";
 
 export function ForgotPasswordForm() {
-    const handleForgotPassword = async (formData: FormData) => {
-        const newForgotPasswordRequest = {
-            email: formData.get("email") as string,
-            password: formData.get("password") as string
-        };
-
-        const result = ForgotPasswordSchema.safeParse(newForgotPasswordRequest);
-        if (!result.success) {
-            toast.warning(result.error.issues[0].message, { position: "top-center" });
-            return;
+    const form = useForm({
+        resolver: zodResolver(ForgotPasswordSchema),
+        defaultValues: {
+            email: "",
+            password: ""
         }
+    });
 
-        const response = await signIn(result.data);
-        if (response?.error) {
-            toast.error(response.error, { position: "top-center" });
-            return;
+    const onSubmit = async (data: ForgotPasswordFormData) => {
+        const result = await forgotPassword(data);
+
+        if (result.success) {
+            form.reset();
+            toast.success("Email sent successfully!", { position: "top-center" });
+            setTimeout(() => window.location.href = "/", 1500);
+        } else {
+            toast.error("Failed sending email.");
         }
-
-        toast.success("Signed in successfully!", { position: "top-center" });
-        setTimeout(() => {
-            window.location.href = "/";
-        }, 1500);
-    }
+    };
 
     return (
-        <form action={handleForgotPassword} className='space-y-4'>
-            <FormField
-                name="email"
-                label="Email Address"
+        <form onSubmit={form.handleSubmit(onSubmit)} className='space-y-4'>
+            <FormInputField
+                control={form.control}
+                controllerName="email"
+                fieldLabel="Email Address"
                 type="email"
                 placeholder="johndoe@gmail.com"
             />
