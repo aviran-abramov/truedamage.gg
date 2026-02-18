6ec9cecffd08df63a89d61fc87076e294b16fcf9
Aviran <aviranabramov@gmail.com>
2026-02-18 18:44:32 +0200

    feat: add createErrorMessage zod helper

---
+++ lib/helpers/zod.ts
@@ -0,0 +1,11 @@
+import * as z from "zod/v4/core";
+
+export const createErrorMessage = (issues: z.$ZodIssue[]): string => {
+    let errorMessage = "";
+
+    issues.forEach((issue) => {
+        errorMessage += `${issue.path}: ${issue.message}`
+    });
+
+    return errorMessage;
+};
