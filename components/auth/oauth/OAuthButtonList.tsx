d550c5e968f8eaf19e0674d1dd401d532385de38
Aviran Abramov <aviranabramov@gmail.com>
2026-02-26 09:57:04 +0200

    chore(oauth): disable fb login, verification needed

--- components/auth/oauth/OAuthButtonList.tsx
+++ components/auth/oauth/OAuthButtonList.tsx
@@ -4,7 +4,8 @@ export function OAuthButtonList() {
   return (
     <div className="space-y-2.5">
       <OAuthButton name="Google" provider="google" />
-      <OAuthButton name="Facebook" provider="facebook" />
+      {/* FB Disabled - Verification Needed */}
+      {/* <OAuthButton name="Facebook" provider="facebook" /> */}
     </div>
   );
 }
