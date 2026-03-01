dfbb16700ee09672df12d5ffc62a4a8d5be6e518
Aviran <aviranabramov@gmail.com>
2026-03-01 20:56:45 +0200

    feat: add teams page

---
+++ app/teams/page.tsx
@@ -0,0 +1,13 @@
+import { AppContainer } from "@/components/layout/AppContainer";
+import { PageTitle } from "@/components/PageTitle";
+
+export default function TeamsPage() {
+
+    return (
+        <AppContainer>
+            <section className="flex flex-col max-w-5xl w-full">
+                <PageTitle>Teams</PageTitle>
+            </section>
+        </AppContainer>
+    )
+}
