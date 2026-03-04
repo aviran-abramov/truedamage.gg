ce924a977849f5abd23e3878635871e1cccc82be
Aviran Abramov <aviranabramov@gmail.com>
2026-03-04 19:34:15 +0200

    feat(GamesFilter): add game removal state

--- components/matches/Filters/GamesFilter.tsx
+++ components/matches/Filters/GamesFilter.tsx
@@ -1,20 +1,33 @@
+"use client";
+
 import { Game } from "@/lib/generated/prisma/client";
 import { FilterTitle } from "./FilterTitle";
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
+import { useState } from "react";
 
 interface GamesFilterProps {
   games: Game[];
 }
 
 export const GamesFilter = ({ games }: GamesFilterProps) => {
+  const [selectedGames, setSelectedGames] = useState(games);
+
+  const handleGameRemovalClick = (gameId: string) => {
+    setSelectedGames((prevState) => prevState.filter((g) => g.id !== gameId));
+  };
+
   return (
     <div className="rounded-sm p-4 text-black dark:text-white text-sm bg-[#F1F1F5] dark:bg-[#191921]">
       <FilterTitle>Games</FilterTitle>
       <ul className="flex items-center flex-wrap gap-2">
-        {games.map((game) => (
+        {selectedGames.map((game) => (
           <li key={game.id}>
-            <Button variant={"outline"} className="cursor-pointer rounded-sm">
+            <Button
+              variant={"outline"}
+              className="cursor-pointer rounded-sm"
+              onClick={() => handleGameRemovalClick(game.id)}
+            >
               <span>✕</span>
               <Image
                 className="dark:invert"
